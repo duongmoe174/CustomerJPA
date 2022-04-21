6704d51d185092e80d90bd277e0ad6f0daa2998e
ThaiDuong <duongmoe174@gmail.com>
2022-04-21 14:34:16 +0700

    done

---
+++ src/main/java/com/duong/customerjpa/reposiroty/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.duong.customerjpa.reposiroty;
+
+import com.duong.customerjpa.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/duong/customerjpa/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.duong.customerjpa.service;
+
+import com.duong.customerjpa.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
